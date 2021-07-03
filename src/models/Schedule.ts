8d321051f5026f0d194a9e811643a5e8355fbe7b
xxeol2 <71129059+xxeol2@users.noreply.github.com>
2021-07-03 17:34:42 +0900

    fix : Schedule.ts에서 ISchedule import 삭제

--- src/models/Schedule.ts
+++ src/models/Schedule.ts
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import { ISchedule, ISchedules } from "../interfaces/ISchedule";
+import { ISchedules } from "../interfaces/ISchedule";
 
 const ScheduleSchema = new mongoose.Schema({
     title : {
