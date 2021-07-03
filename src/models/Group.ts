07a2ffded8dfea6a93bdaa1d848d982e25c98e49
변주현 <wngus4296@swu.ac.kr>
2021-07-03 17:56:22 +0900

    fix: Group.ts에 ISchedule, ISchedules import 삭제, group host -> user 참조

--- src/models/Group.ts
+++ src/models/Group.ts
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
 import { IGroup } from "../interfaces/IGroup";
-import { ISchedule, ISchedules } from "../interfaces/ISchedule";
 
 const GroupSchema = new mongoose.Schema({
     members: [{
@@ -24,8 +23,8 @@ const GroupSchema = new mongoose.Schema({
         required: true
     },
     host: {
-        type: String,
-        required: true
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: "User"
     },
     travelName: {
         type: String,
