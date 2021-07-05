4c62e256ca164bbb29168b691930ec7a2376eccf
jokj624 <jokj624@gmail.com>
2021-07-06 04:58:50 +0900

    feat: kakao 로그인 라우터 구현

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@
 import { Router } from 'express';
 import travelRouter from './travelRouter';
 import mainRouter from './mainRouter';
+import kakaoRouter from './kakaoRouter';
 
 const router = Router();
 
 router.use('/travel', travelRouter);
 router.use('/travel', mainRouter);
+router.use('/auth', kakaoRouter);
 
 export default router;
