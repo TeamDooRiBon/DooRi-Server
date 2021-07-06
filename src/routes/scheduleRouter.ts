6fa42bb0b14b2ae4d03fdd9c37d1e167d2176339
변주현 <wngus4296@swu.ac.kr>
2021-07-06 20:47:41 +0900

    fix: group-id -> groupId

--- src/routes/scheduleRouter.ts
+++ src/routes/scheduleRouter.ts
@@ -4,7 +4,7 @@ import auth from '../middleware/auth';
 
 const router = Router();
 
-router.post('/:group-id', auth, scheduleController.makeSchedule);
+router.post('/:groupId', auth, scheduleController.makeSchedule);
 router.get('/daily/:groupId/:date', auth, scheduleController.getDailySchedule);
 router.get('/:groupId/:scheduleId', auth, scheduleController.getDailySchedule);
 
