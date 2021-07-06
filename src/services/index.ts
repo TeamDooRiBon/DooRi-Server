addb5b05fd34a10ea14da9a17671bc5e284ef9fe
jokj624 <jokj624@gmail.com>
2021-07-06 17:57:33 +0900

    feat: userService 추가 및 mainService 내 findUser 이동

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,9 +1,11 @@
 import groupService from "./groupService"
 import mainService from "./mainService"
 import scheduleService from "./scheduleService"
+import userService from "./userService"
 //service index file
 export {
     groupService,
     mainService,
-    scheduleService
+    scheduleService,
+    userService
 }
