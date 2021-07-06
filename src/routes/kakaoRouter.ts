c82dd2734300a8ef277887ca0a6c6ce86513a0f9
jokj624 <jokj624@gmail.com>
2021-07-06 20:21:32 +0900

    style: 카카오 로그인 테스트용 주석

--- src/routes/kakaoRouter.ts
+++ src/routes/kakaoRouter.ts
@@ -4,7 +4,7 @@ import { kakaoController } from '../controllers';
 const router = Router();
 /**
  * 클라이언트 없이 서버 테스트 시에는 아래 주석을 풀고 진행하세요.
- * router.get('/kakao/callback', kakaoController.getKakaoUser);
-**/
+  router.get('/kakao/callback', kakaoController.getKakaoUser);
+*/
 router.get('/user', kakaoController.getKakaoUser);
 export default router;
