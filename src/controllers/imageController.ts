5cb445f47a3c61bce02da6b98c510840be91af5a
변주현 <wngus4296@swu.ac.kr>
2021-07-06 16:44:26 +0900

    feat: image Router, Controller 생성 및 src/routes/index.ts에 image Router 추가

---
+++ src/controllers/imageController.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from "express";
+const sc = require('../modules/statusCode');
+const image = require('../modules/image');
+
+const getImages = async (req: Request, res: Response) => {
+    try{
+        return res.status(200).json({
+            status: sc.OK,
+            success: true,
+            message: "전체 이미지 조회 성공",
+            data: image
+        })
+    } catch (err) {
+        console.log(err);
+        res.status(sc.INTERNAL_SERVER_ERROR).json({ status: sc.INTERNAL_SERVER_ERROR, success: false, message: "서버 내부 오류" });
+    }
+}
+
+export default {
+    getImages
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@
 import { Router } from 'express';
 import travelRouter from './travelRouter';
 import mainRouter from './mainRouter';
+import imageRouter from './imageRouter';
 
 const router = Router();
 
 router.use('/travel', travelRouter);
 router.use('/travel', mainRouter);
+router.use('/image', imageRouter);
 
 export default router;
