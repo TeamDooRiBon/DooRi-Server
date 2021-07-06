026f323bf2751ae988a00dcafe62a8288d9f84fc
jokj624 <jokj624@gmail.com>
2021-07-06 19:37:34 +0900

    feat: 여행 그룹 참여 코드 추가

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import travelController from "./travelController";
 import scheduleController from "./scheduleController";
 import kakaoController from "./kakaoController";
@@ -6,14 +5,5 @@ import kakaoController from "./kakaoController";
 export {
     travelController,
     scheduleController,
-=======
-import travelController from "./travelController"
-import mainController from "./mainController"
-import kakaoController from "./kakaoController"
-// controller index file
-export {
-    travelController,
-    mainController,
->>>>>>> 1c653b7340d454970ed60e8024b4c7783bc403d8
     kakaoController
 }
