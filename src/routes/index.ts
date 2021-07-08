20ac7e83f12f7d317177aa2d0f9015eab1201ac5
변주현 <wngus4296@swu.ac.kr>
2021-07-08 15:39:33 +0900

    feat: myPage조회 기능 생성

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,11 +4,13 @@ import travelRouter from './travelRouter';
 import imageRouter from './imageRouter';
 import scheduleRouter from './scheduleRouter';
 import kakaoRouter from './kakaoRouter';
+import userRouter from './userRouter';
 const router = Router();
 
 router.use('/travel', travelRouter);
 router.use('/image', imageRouter);
 router.use('/schedule', scheduleRouter); 
 router.use('/auth', kakaoRouter);
+router.use('/user', userRouter);
 
 export default router;
