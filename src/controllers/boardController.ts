194266496ed9ad934d68b9bf2ba9a7a37f1991cb
변주현 <wngus4296@swu.ac.kr>
2021-07-09 02:55:30 +0900

    [GET] 보드 태그별 조회 생성

--- src/controllers/boardController.ts
+++ src/controllers/boardController.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from "express";
 const sc = require('../modules/statusCode');
 const { validationResult } = require('express-validator');
 const tagMatch = require('../modules/tag');
-import { boardService, groupService } from "../services";
+import { boardService, groupService, userService } from "../services";
 import mongoose from "mongoose";
 import Schedule from "../models/Schedule";
 
@@ -13,11 +13,11 @@ import Schedule from "../models/Schedule";
  */
 const makeBoard = async (req: Request, res: Response) => {
     const error = validationResult(req);
-    if(!error.isEmpty()) {
-        return res.status(sc.BAD_REQUEST).json({ 
-            status: sc.BAD_REQUEST, 
-            success: false, 
-            message: "필요한 값이 없습니다." 
+    if (!error.isEmpty()) {
+        return res.status(sc.BAD_REQUEST).json({
+            status: sc.BAD_REQUEST,
+            success: false,
+            message: "필요한 값이 없습니다."
         });
     }
     try {
@@ -26,9 +26,9 @@ const makeBoard = async (req: Request, res: Response) => {
         const tag = tagMatch[req.params.tag];
         const groupId = req.params.groupId;
         const group = await groupService.findGroupById(groupId);
-        
-        const data = { 
-            groupId : mongoose.Types.ObjectId(groupId),
+
+        const data = {
+            groupId: mongoose.Types.ObjectId(groupId),
             writer,
             tag,
             content
@@ -47,16 +47,58 @@ const makeBoard = async (req: Request, res: Response) => {
         });
     } catch (error) {
         console.log(error);
-        res.status(sc.INTERNAL_SERVER_ERROR).json({ 
-            status: sc.INTERNAL_SERVER_ERROR, 
-            success: false, 
-            message: "서버 내부 오류" 
+        res.status(sc.INTERNAL_SERVER_ERROR).json({
+            status: sc.INTERNAL_SERVER_ERROR,
+            success: false,
+            message: "서버 내부 오류"
         });
     }
-};  
+};
 
-
+/**
+ *  @route GET /board/:groupId/:tag
+ *  @desc Get board By tag
+ *  @access Private
+ */
+const getBoard = async (req: Request, res: Response) => {
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+        return res.status(sc.BAD_REQUEST).json({
+            status: sc.BAD_REQUEST,
+            success: false,
+            message: "필요한 값이 없습니다."
+        });
+    }
+    try {
+        const tag = tagMatch[req.params.tag];
+        const boardList = await boardService.findBoard(req.params.groupId, tag);
+        let data = []
+        boardList.map((b) => {
+            if (b.tag == tag) {
+                let pushData = {
+                    "writer": b.writer, // 클라에서 user이름 찾는 걸로 다시 요청해주셔야할 것 같습니다. await가 안먹네요,, 어떻게 하면 이름이 나올까요
+                    "content": b.content 
+                }
+                data.unshift(pushData);
+            }
+        });
+        return res.status(sc.OK).json({
+            status: sc.OK,
+            success: true,
+            message: "그룹별 보드 조회 성공",
+            data: data
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(sc.INTERNAL_SERVER_ERROR).json({
+            status: sc.INTERNAL_SERVER_ERROR,
+            success: false,
+            message: "서버 내부 오류"
+        });
+    }
+};
 
 export default {
-    makeBoard
-} 
+    makeBoard,
+    getBoard
+}
--- src/services/boardService.ts
+++ src/services/boardService.ts
@@ -1,9 +1,10 @@
 import Board from "../models/Board";
 import Group from "../models/Group";
 import { IBoardInputDTO } from "../interfaces/IBoard";
+import groupService from "./groupService";
 
 
-const createBoard = async (data : IBoardInputDTO) => {
+const createBoard = async (data: IBoardInputDTO) => {
     const {
         groupId, writer, tag, content
     } = data;
@@ -43,8 +44,19 @@ const addBoard = async (data : IBoardInputDTO) => {
     } 
 };
 
+const findBoard = async (groupId: String, tagData: String) => {
+    try {
+        const group = await groupService.findGroupById(groupId);
+        const boardList = await Board.findOne(group.boards);
+        return boardList.post;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    } 
+};
 
 export default {
     createBoard,
-    addBoard
+    addBoard,
+    findBoard
 } 
