68acb40ec679fd45df47dc9b588c6082ceaa5f8e
wngus4296 <wngus4296@swu.ac.kr>
2021-07-09 18:51:43 +0900

    fix: map -> 쿼리 수정

--- src/services/boardService.ts
+++ src/services/boardService.ts
@@ -2,6 +2,7 @@ import Board from "../models/Board";
 import Group from "../models/Group";
 import { IBoardInputDTO } from "../interfaces/IBoard";
 import groupService from "./groupService";
+import mongoose from "mongoose";
 
 
 const createBoard = async (data: IBoardInputDTO) => {
@@ -9,15 +10,15 @@ const createBoard = async (data: IBoardInputDTO) => {
         groupId, writer, tag, content
     } = data;
 
-    try { 
+    try {
         const boards = new Board({
-            post : [{ 
+            post: [{
                 writer,
                 tag,
-                content    
+                content
             }]
         });
-        await Group.findByIdAndUpdate(groupId , { $set : { boards : boards._id }});
+        await Group.findByIdAndUpdate(groupId, { $set: { boards: boards._id } });
         await boards.save();
         return;
     } catch (error) {
@@ -26,36 +27,43 @@ const createBoard = async (data: IBoardInputDTO) => {
     }
 }
 
-const addBoard = async (data : IBoardInputDTO) => {
+const addBoard = async (data: IBoardInputDTO) => {
     const {
         groupId, writer, tag, content
     } = data;
     try {
         const group = await Group.findById(groupId);
-        await Board.findByIdAndUpdate(group.boards, { $push : { post : {
-            writer,
-            tag,
-            content
-        }}});
+        await Board.findByIdAndUpdate(group.boards, {
+            $push: {
+                post: {
+                    writer,
+                    tag,
+                    content
+                }
+            }
+        });
         return;
     } catch (error) {
         console.log(error);
         throw error;
-    } 
+    }
 };
 
-const findBoard = async (boardsId: String, tagData: String) => {
+const findBoard = async (boardsId: mongoose.Types.ObjectId, tagData: String) => {
     try {
-        const boardList = await Board.findOne({_id: boardsId});
-        return boardList;
+        const boardList = await Board.aggregate([{ $unwind: '$post' }, {
+            $match: { _id: boardsId, "$expr": { "$eq": ["$post.tag", tagData] } }
+        }]);
+
+        console.log(boardList);
     } catch (error) {
         console.log(error);
         throw error;
-    } 
+    }
 };
 
 export default {
     createBoard,
     addBoard,
     findBoard
-} 
+}
