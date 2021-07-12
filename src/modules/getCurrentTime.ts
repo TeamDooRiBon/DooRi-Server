9d8b7205e9e8d5f741ef0b9b07f76988a34b6053
jokj624 <jokj624@gmail.com>
2021-07-12 18:38:24 +0900

    fix: current time 모듈 함수 날짜 구하는 코드 수정

--- src/modules/getCurrentTime.ts
+++ src/modules/getCurrentTime.ts
@@ -2,7 +2,7 @@ module.exports = () => {
     const time = new Date();
     const year = time.getFullYear();
     const month = time.getMonth();
-    const date = time.getDate() - 1;
+    const date = time.getDate();
     const hour = time.getHours();
     const minute = time.getMinutes();
     const result = new Date(Date.UTC(year, month, date, hour, minute));
