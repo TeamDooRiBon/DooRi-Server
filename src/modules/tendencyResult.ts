f11d4476c8711bcf55a4c303bca392251b0e8a9b
jokj624 <jokj624@gmail.com>
2021-07-17 03:54:30 +0900

    fix: iOS 성향 결과 이미지 url 수정

--- src/modules/tendencyResult.ts
+++ src/modules/tendencyResult.ts
@@ -4,7 +4,7 @@ module.exports = {
         tag: [
             "#새로운걸원해", "#고민보다GO", "#무한도전"
         ],
-        iOSResultImage: "https://dooribon.s3.ap-northeast-2.amazonaws.com/result/iOS/%EC%97%B4%EC%A0%95%EC%A0%81%EC%9D%B8+%ED%83%90%ED%97%98%EA%B0%80-%ED%86%B5%EC%9D%B4%EB%AF%B8%EC%A7%80.png",
+        iOSResultImage: "https://dooribon.s3.ap-northeast-2.amazonaws.com/result/iOS/8xResult/%EC%97%B4%EC%A0%95%EC%A0%81%EC%9D%B8+%ED%83%90%ED%97%98%EA%B0%80-%ED%86%B5%EC%9D%B4%EB%AF%B8%EC%A7%80.png",
         aOSResultImage: "https://dooribon.s3.ap-northeast-2.amazonaws.com/result/android/%E1%84%8B%E1%85%A7%E1%86%AF%E1%84%8C%E1%85%A5%E1%86%BC%E1%84%8C%E1%85%A5%E1%86%A8%E1%84%8B%E1%85%B5%E1%86%AB+%E1%84%90%E1%85%A1%E1%86%B7%E1%84%92%E1%85%A5%E1%86%B7%E1%84%80%E1%85%A1.png",
         thumbnail: "https://dooribon.s3.ap-northeast-2.amazonaws.com/resultCard/img_card_yeoljeong.png"
     },
@@ -13,7 +13,7 @@ module.exports = {
         tag: [
             "#계획도장깨기", "#여행리더", "#완벽주의자"
         ],
-        iOSResultImage: "https://dooribon.s3.ap-northeast-2.amazonaws.com/result/iOS/%EC%B2%A0%EB%91%90%EC%B2%A0%EB%AF%B8+%EA%B3%84%ED%9A%8D%EA%B0%80-%ED%86%B5%EC%9D%B4%EB%AF%B8%EC%A7%80.png",
+        iOSResultImage: "https://dooribon.s3.ap-northeast-2.amazonaws.com/result/iOS/8xResult/%EC%B2%A0%EB%91%90%EC%B2%A0%EB%AF%B8+%EA%B3%84%ED%9A%8D%EA%B0%80-%ED%86%B5%EC%9D%B4%EB%AF%B8%EC%A7%80.png",
         aOSResultImage: "https://dooribon.s3.ap-northeast-2.amazonaws.com/result/android/%E1%84%8E%E1%85%A5%E1%86%AF%E1%84%83%E1%85%AE%E1%84%8E%E1%85%A5%E1%86%AF%E1%84%86%E1%85%B5+%E1%84%80%E1%85%A8%E1%84%92%E1%85%AC%E1%86%A8%E1%84%80%E1%85%A1.png",
         thumbnail: "https://dooribon.s3.ap-northeast-2.amazonaws.com/resultCard/img_card_chuldoo.png"
     },
@@ -22,7 +22,7 @@ module.exports = {
         tag: [
             "#놀땐놀고", "#쉴땐쉬자", "#마이웨이"
         ],
-        iOSResultImage: "https://dooribon.s3.ap-northeast-2.amazonaws.com/result/iOS/%EC%A6%89%ED%9D%A5%EC%A0%81%EC%9D%B8%ED%96%89%EB%8F%99%EA%B0%80-%ED%86%B5%EC%9D%B4%EB%AF%B8%EC%A7%80.png",
+        iOSResultImage: "https://dooribon.s3.ap-northeast-2.amazonaws.com/result/iOS/8xResult/%EC%A6%89%ED%9D%A5%EC%A0%81%EC%9D%B8%ED%96%89%EB%8F%99%EA%B0%80-%ED%86%B5%EC%9D%B4%EB%AF%B8%EC%A7%80.png",
         aOSResultImage: "https://dooribon.s3.ap-northeast-2.amazonaws.com/result/android/%E1%84%8C%E1%85%B3%E1%86%A8%E1%84%92%E1%85%B3%E1%86%BC%E1%84%8C%E1%85%A5%E1%86%A8%E1%84%8B%E1%85%B5%E1%86%AB+%E1%84%92%E1%85%A2%E1%86%BC%E1%84%83%E1%85%A9%E1%86%BC%E1%84%80%E1%85%A1.png",
         thumbnail: "https://dooribon.s3.ap-northeast-2.amazonaws.com/resultCard/img_card_zeukheung.png"
     },
@@ -31,7 +31,7 @@ module.exports = {
         tag: [
             "#흘러가는대로", "#감성충만", "#낭만을찾아서"
         ],
-        iOSResultImage: "https://dooribon.s3.ap-northeast-2.amazonaws.com/result/iOS/%EB%82%AD%EB%A7%8C%EC%A0%81%EC%9D%B8%EB%B0%A9%EB%9E%91%EC%9E%90-%ED%86%B5%EC%9D%B4%EB%AF%B8%EC%A7%80.png",
+        iOSResultImage: "https://dooribon.s3.ap-northeast-2.amazonaws.com/result/iOS/8xResult/%EB%82%AD%EB%A7%8C%EC%A0%81%EC%9D%B8%EB%B0%A9%EB%9E%91%EC%9E%90-%ED%86%B5%EC%9D%B4%EB%AF%B8%EC%A7%80.png",
         aOSResultImage: "https://dooribon.s3.ap-northeast-2.amazonaws.com/result/android/%E1%84%82%E1%85%A1%E1%86%BC%E1%84%86%E1%85%A1%E1%86%AB%E1%84%8C%E1%85%A5%E1%86%A8%E1%84%8B%E1%85%B5%E1%86%AB+%E1%84%87%E1%85%A1%E1%86%BC%E1%84%85%E1%85%A1%E1%86%BC%E1%84%8C%E1%85%A1.png",
         thumbnail: "https://dooribon.s3.ap-northeast-2.amazonaws.com/resultCard/img_card_nangman.png"
     },
@@ -40,7 +40,7 @@ module.exports = {
         tag: [
             "#나만의세상", "#마음속에저장", "#사유의즐거움"
         ],
-        iOSResultImage: "https://dooribon.s3.ap-northeast-2.amazonaws.com/result/iOS/%EA%B0%90%EC%84%B1%EC%A0%81%EC%9D%B8%EA%B4%80%EC%B0%B0%EC%9E%90-%ED%86%B5%EC%9D%B4%EB%AF%B8%EC%A7%80.png",
+        iOSResultImage: "https://dooribon.s3.ap-northeast-2.amazonaws.com/result/iOS/8xResult/%EA%B0%90%EC%84%B1%EC%A0%81%EC%9D%B8%EA%B4%80%EC%B0%B0%EC%9E%90-%ED%86%B5%EC%9D%B4%EB%AF%B8%EC%A7%80.png",
         aOSResultImage: "https://dooribon.s3.ap-northeast-2.amazonaws.com/result/android/%E1%84%80%E1%85%A1%E1%86%B7%E1%84%89%E1%85%A5%E1%86%BC%E1%84%8C%E1%85%A5%E1%86%A8%E1%84%8B%E1%85%B5%E1%86%AB+%E1%84%80%E1%85%AA%E1%86%AB%E1%84%8E%E1%85%A1%E1%86%AF%E1%84%8C%E1%85%A1.png",
         thumbnail: "https://dooribon.s3.ap-northeast-2.amazonaws.com/resultCard/img_card_gamsung.png"
     },
@@ -49,7 +49,7 @@ module.exports = {
         tag: [
             "#리액션자판기", "#호불호없음", "#배려왕"
         ],
-        iOSResultImage: "https://dooribon.s3.ap-northeast-2.amazonaws.com/result/iOS/%EB%AC%B4%EC%97%87%EC%9D%B4%EB%93%A0%EC%A2%8B%EC%95%84%EB%B4%87-%ED%86%B5%EC%9D%B4%EB%AF%B8%EC%A7%80.png",
+        iOSResultImage: "https://dooribon.s3.ap-northeast-2.amazonaws.com/result/iOS/8xResult/%EB%AC%B4%EC%97%87%EC%9D%B4%EB%93%A0%EC%A2%8B%EC%95%84%EB%B4%87-%ED%86%B5%EC%9D%B4%EB%AF%B8%EC%A7%80.png",
         aOSResultImage: "https://dooribon.s3.ap-northeast-2.amazonaws.com/result/android/%E1%84%86%E1%85%AE%E1%84%8B%E1%85%A5%E1%86%BA%E1%84%8B%E1%85%B5%E1%84%83%E1%85%B3%E1%86%AB+%E1%84%8C%E1%85%A9%E1%87%82%E1%84%8B%E1%85%A1%E1%84%87%E1%85%A9%E1%86%BA.png",
         thumbnail: "https://dooribon.s3.ap-northeast-2.amazonaws.com/resultCard/img_card_goodgood.png"
     },
@@ -58,7 +58,7 @@ module.exports = {
         tag: [
             "#친화력갑", "#분위기메이커", "#노는게제일좋아"
         ],
-        iOSResultImage: "https://dooribon.s3.ap-northeast-2.amazonaws.com/result/iOS/%ED%85%90%EC%85%98%ED%8F%AD%EB%B0%9C%ED%95%B5%EC%9D%B8%EC%8B%B8-%ED%86%B5%EC%9D%B4%EB%AF%B8%EC%A7%80.png",
+        iOSResultImage: "https://dooribon.s3.ap-northeast-2.amazonaws.com/result/iOS/8xResult/%ED%85%90%EC%85%98%ED%8F%AD%EB%B0%9C%ED%95%B5%EC%9D%B8%EC%8B%B8-%ED%86%B5%EC%9D%B4%EB%AF%B8%EC%A7%80.png",
         aOSResultImage: "https://dooribon.s3.ap-northeast-2.amazonaws.com/result/android/%E1%84%90%E1%85%A6%E1%86%AB%E1%84%89%E1%85%A7%E1%86%AB%E1%84%91%E1%85%A9%E1%86%A8%E1%84%87%E1%85%A1%E1%86%AF+%E1%84%92%E1%85%A2%E1%86%A8%E1%84%8B%E1%85%B5%E1%86%AB%E1%84%8A%E1%85%A1.png",
         thumbnail: "https://dooribon.s3.ap-northeast-2.amazonaws.com/resultCard/img_card_tensionboom.png"
     },
@@ -67,7 +67,7 @@ module.exports = {
         tag: [
             "#카르페디엠", "#힐링이좋아", "#현지인바이브"
         ],
-        iOSResultImage: "https://dooribon.s3.ap-northeast-2.amazonaws.com/result/iOS/%EC%97%AC%EC%9C%A0%EB%A7%8C%EB%95%85%EB%B2%A0%EC%A7%B1%EC%9D%B4-%ED%86%B5%EC%9D%B4%EB%AF%B8%EC%A7%80.png",
+        iOSResultImage: "https://dooribon.s3.ap-northeast-2.amazonaws.com/result/iOS/8xResult/%EC%97%AC%EC%9C%A0%EB%A7%8C%EB%95%85%EB%B2%A0%EC%A7%B1%EC%9D%B4-%ED%86%B5%EC%9D%B4%EB%AF%B8%EC%A7%80.png",
         aOSResultImage: "https://dooribon.s3.ap-northeast-2.amazonaws.com/result/android/%E1%84%8B%E1%85%A7%E1%84%8B%E1%85%B2%E1%84%86%E1%85%A1%E1%86%AB%E1%84%84%E1%85%A1%E1%86%BC+%E1%84%87%E1%85%A6%E1%84%8D%E1%85%A1%E1%86%BC%E1%84%8B%E1%85%B5.png",
         thumbnail: "https://dooribon.s3.ap-northeast-2.amazonaws.com/resultCard/img_card_yeoyoomanddang.png"
     },
