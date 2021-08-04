303318665cea51047f3aedf93a4884174502e8a7
jokj624 <jokj624@gmail.com>
2021-08-05 01:23:35 +0900

    fix: 쿼리 속도 개선 - 비동기 병렬 처리

--- src/services/mainService.ts
+++ src/services/mainService.ts
@@ -8,9 +8,11 @@ const findTravelByDate = async (data: userSearchInput) => {
         const user = await User.findOne({_id: data});
         const groupIds = user.groups;
 
-        const nowTravels = await Group.find({_id : {$in : groupIds}, startDate : {$lte : date}, endDate : {$gte : date}}).sort({startDate : 1}).populate("members", ["name"]);
-        const comeTravels = await Group.find({_id : {$in : groupIds}, startDate : {$gte : date}}).sort({startDate : 1}).populate("members", ["name"]);
-        const endTravels = await Group.find({_id : {$in : groupIds}, endDate : {$lte : date}}).sort({startDate : 1}).populate("members", ["name"]);
+        const [nowTravels, comeTravels, endTravels]= await Promise.all([
+            Group.find({ _id: { $in: groupIds }, startDate: { $lte: date }, endDate: { $gte: date } }).sort({ startDate: 1 }).populate("members", ["name"]),
+            Group.find({ _id: { $in: groupIds }, startDate: { $gte: date } }).sort({ startDate: 1 }).populate("members", ["name"]),
+            Group.find({ _id: { $in: groupIds }, endDate: { $lte: date } }).sort({ startDate: 1 }).populate("members", ["name"]),
+        ]); // 병렬 처리
 
         return {nowTravels, comeTravels, endTravels};
     } catch (error) {
